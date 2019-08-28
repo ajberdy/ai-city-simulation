d0613a0841f41ea304cb9febca22fa37a36ae3a1
Aaron Berdy <aaronberdy@ucla.edu>
2019-08-28 11:36:19 -0700

    graphql test

---
+++ src/search_training_facility/graphql_utils.py
@@ -0,0 +1,24 @@
+from sgqlc.endpoint.http import HTTPEndpoint
+
+url = 'https://ai-city-server.herokuapp.com'
+headers = {'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjanpxMmwx'
+                            'bDhjdjN3MDc5NGFpZ2JqbjZnIiwiaWF0IjoxNTY2Njg0NTYyfQ.MEHsGyvdmR'
+                            'Hj7zQh0t8b7FLXPf4Z_KbDcDeLlJ_Hbj8'
+           }
+
+query = """
+    query {
+      searchTf {
+        id
+        roadGraph {
+          name
+        }
+      }
+    }
+"""
+variables = {}
+
+endpoint = HTTPEndpoint(url, headers)
+data = endpoint(query, variables)
+
+print(data)
